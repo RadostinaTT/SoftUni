820f1aadd5152073d852769848c4a1a755f6dc2e
Radostina Todorova <radtod77@icloud.com>
2023-09-26 22:16:15 +0300

    initial commit

---
+++ C# Advanced/MultidimensionalArrays-Exercise/SnakeMoves/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int matrixRow = dimensions[0];
+            int matrixCol = dimensions[1];
+            char[,] matrix = new char[matrixRow, matrixCol];
+            string snake = Console.ReadLine();
+            int indexString = 0;
+            int counterRows = 0;
+            while (counterRows < matrixRow)
+            {
+                if (counterRows % 2 == 0)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        matrix[counterRows, col] = snake[indexString];
+                        indexString++;
+                        if (indexString == snake.Length)
+                        {
+                            indexString = 0;
+                        }
+                    }
+                    counterRows++;
+                }
+                else
+                {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        matrix[counterRows, col] = snake[indexString];
+                        indexString++;
+                        if (indexString == snake.Length)
+                        {
+                            indexString = 0;
+                        }
+                    }
+                    counterRows++;
+                }
+            }
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/MultidimensionalArrays-Exercise/SquaresInMatrix/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rowMatrix = dimensions[0];
+            int colMatrix = dimensions[1];
+            int controlNumber = 2;
+            char[,] matrix = new char[rowMatrix, colMatrix];
+            for (int row = 0; row < rowMatrix; row++)
+            {
+                char[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < colMatrix; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            int countEqualMatrixes = 0;
+            for (int row = 0; row < rowMatrix - 1; row++)
+            {
+                for (int col = 0; col < colMatrix - 1; col++)
+                {
+                    char currentChar = matrix[row, col];
+                    int countdownEquals = controlNumber * controlNumber;
+                    for (int subrow = 0; subrow < controlNumber; subrow++)
+                    {
+                        for (int subcol = 0; subcol < controlNumber; subcol++)
+                        {
+                            if (matrix[row + subrow,col + subcol] == currentChar)
+                            {
+                                countdownEquals--;
+                            }
+                        }
+                    }
+                    if (countdownEquals == 0)
+                    {
+                        countEqualMatrixes++;
+                    }
+                }
+            }
+            Console.WriteLine(countEqualMatrixes);
+        }
+    }
+}
