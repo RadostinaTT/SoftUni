e6b3329ec3228fe0d29157490f13218f17a04ccd
Radostina Todorova <radtod77@icloud.com>
2023-09-26 21:35:30 +0300

    initial commit

---
+++ Generics-Exercise/Generics-Exercise/StartUp.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Generics_Exercise
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Box<double> box = new Box<double>();
+
+            int counter = int.Parse(Console.ReadLine());
+            for (int i = 0; i < counter; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                box.Value.Add(input);
+            }
+
+            double command = double.Parse(Console.ReadLine());
+
+            var result = box.GreaterValuesThan(command);
+            //int[] command = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            //int firstIndex = command[0];
+            //int secondIndex = command[1];
+            //box.Swap(firstIndex, secondIndex);
+
+            Console.WriteLine(result);
+
+
+            
+        }
+    }
+}
---
+++ Generics-Exercise/Tuple/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] inputPerson = Console.ReadLine().Split();
+            string fullName = inputPerson[0] + " " + inputPerson[1];
+            string town = inputPerson[2];
+
+            string[] inputBeer = Console.ReadLine().Split();
+            string name = inputBeer[0];
+            int beerLiters = int.Parse(inputBeer[1]);
+
+            string[] inputIntDouble = Console.ReadLine().Split();
+            int numberInt = int.Parse(inputIntDouble[0]);
+            double numberDouble = double.Parse(inputIntDouble[1]);
+
+            var person = new Tuple<string, string>(fullName, town);
+            var beer = new Tuple<string, int>(name, beerLiters);
+            var intDouble = new Tuple<int, double>(numberInt, numberDouble);
+
+            Console.WriteLine(person);
+            Console.WriteLine(beer);
+            Console.WriteLine(intDouble);
+        }
+    }
+}
