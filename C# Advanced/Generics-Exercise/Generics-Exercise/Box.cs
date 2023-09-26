1d9afda95d183a4a5976dcf0d45a477c9224afe1
Radostina Todorova <radtod77@icloud.com>
2023-09-26 21:38:59 +0300

    initial commit

---
+++ C# Advanced/Generics-Exercise/Generics-Exercise/Box.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics_Exercise
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        public Box()
+        {
+            this.Value = new List<T>();
+        }
+
+        public List<T> Value { get; set; }
+        public int GreaterValuesThan(T targetItem)
+        {
+            int counter = 0;
+            foreach (var item in this.Value)
+            {
+                if (item.CompareTo(targetItem) > 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        public void Swap(int a, int b)
+        {
+            bool IsInRange = a >= 0 && a < this.Value.Count &&
+                b >= 0 && b < this.Value.Count;
+
+            if (!IsInRange)
+            {
+                throw new InvalidOperationException("Values are not in range");
+            }
+
+            T tempValue = this.Value[a];
+            this.Value[a] = this.Value[b];
+            this.Value[b] = tempValue;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var value in this.Value)
+            {
+                sb.AppendLine($"{value.GetType()}: {value}");
+            }
+
+            string result = sb.ToString().TrimEnd();
+
+            return result;
+        }
+    }
+}
---
+++ C# Advanced/Generics-Exercise/Threeuple/StartUp.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Threeuple
+{
+    using System;
+    using System.Linq;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] inputPerson = Console.ReadLine().Split();
+            string fullName = inputPerson[0] + " " + inputPerson[1];
+            string address = inputPerson[2];
+            string town = string.Join(" ", inputPerson.Skip(3));
+
+            string[] inputBeer = Console.ReadLine().Split();
+            string name = inputBeer[0];
+            int beerLiters = int.Parse(inputBeer[1]);
+            string drunkOrNot = inputBeer[2];
+            bool IsDrunk = drunkOrNot == "drunk" ? true : false;
+            
+            string[] bankAccount = Console.ReadLine().Split();
+            string nameAccount = bankAccount[0];
+            double balance = double.Parse(bankAccount[1]);
+            string nameBank = bankAccount[2];
+
+            var person = new Threeuple<string, string, string>(fullName, address, town);
+            var beer = new Threeuple<string, int, bool>(name, beerLiters, IsDrunk);
+            var bankDetails = new Threeuple<string, double, string>(nameAccount, balance, nameBank);
+
+            Console.WriteLine(person);
+            Console.WriteLine(beer);
+            Console.WriteLine(bankDetails);
+        }
+    }
+}
