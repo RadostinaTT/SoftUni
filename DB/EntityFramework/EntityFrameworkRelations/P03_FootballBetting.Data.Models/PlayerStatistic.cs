bb8cac0a4794830889c97284652f8ee40827598d
Radostina Todorova <radtod77@icloud.com>
2023-09-27 17:00:44 +0300

    initial commit

---
+++ DB/EntityFramework/EntityFrameworkRelations/P03_FootballBetting.Data.Models/PlayerStatistic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class PlayerStatistic
+    {
+        //GameId, PlayerId, ScoredGoals, Assists, MinutesPlayed
+        public PlayerStatistic()
+        {
+            this.PlayerStatistics = new HashSet<PlayerStatistic>();
+        }
+        public int GameId { get; set; }
+        public virtual Game Game { get; set; }
+        public int PlayerId { get; set; }
+        public virtual Player Player { get; set; }
+        public byte ScoredGoals { get; set; }
+        public byte Assists { get; set; }
+        public byte MinutesPlayed { get; set; }
+
+        public virtual ICollection<PlayerStatistic> PlayerStatistics { get; set; }
+    }
+}
