75b450e35a515569f342760626e01279e458969b
Radostina Todorova <radtod77@icloud.com>
2023-09-27 16:57:32 +0300

    initial commit

---
+++ DB/EntityFramework/EntityFrameworkLinq-Exercise/P03_SalesDatabase/Data/Config/Connection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.Data.Config
+{
+    public static class Connection
+    {
+        public static string ConnectionString = @"Server=LAPTOP-0TN9PVA8;Database=SalesDb;Integrated Security=True;";
+    }
+}
---
+++ DB/EntityFramework/EntityFrameworkLinq-Exercise/P03_SalesDatabase/Data/Seeding/StoreSeeder.cs
@@ -0,0 +1,36 @@
+﻿using P03_SalesDatabase.Data.Models;
+using P03_SalesDatabase.Data.Seeding.Contracts;
+using P03_SalesDatabase.IOManagement.Contracts;
+
+namespace P03_SalesDatabase.Data.Seeding
+{
+    public class StoreSeeder : ISeeder
+    {
+        private readonly SalesContext dbContext;
+        private readonly IWriter writer; 
+
+        public StoreSeeder(SalesContext context, IWriter writer)
+        {
+            this.dbContext = context;
+            this.writer = writer;
+        }
+        public void Seed()
+        {
+            Store[] stores = new Store[]
+            {
+                new Store{Name = "PCTech Sofia"},
+                new Store{Name = "PCTech Varna"},
+                new Store{Name = "PCTech Plovdiv"},
+                new Store{Name = "InovativeTech Sofia"},
+                new Store{Name = "InovativeTech Plovdiv"},
+                new Store{Name = "InovativeTech Varna"}
+            };
+
+            this.dbContext.Stores.AddRange(stores);
+            
+            this.dbContext.SaveChanges();
+            
+            this.writer.WriteLine($"{stores.Length} stores were added to the database!");
+        }
+    }
+}
